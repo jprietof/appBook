d7a068ca80db70d41bdec6e3ea81782c186e5d7a
Jorge Prieto <jprietof@outlook.es>
2021-10-15 21:16:31 -0500

    Creando el proyecto book

---
+++ src/main/java/com/company/books/backend/ApirestBookApplication.java
@@ -0,0 +1,13 @@
+package com.company.books.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBookApplication.class, args);
+	}
+
+}
