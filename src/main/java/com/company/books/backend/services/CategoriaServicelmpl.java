62681fcd1cef07250dd61a4a14e9cc5869ebc43d
Jorge Prieto <jprietof@outlook.es>
2021-10-17 14:15:32 -0500

    Se creo la interfaz y la clase del Service Categoria

---
+++ src/main/java/com/company/books/backend/services/CategoriaServicelmpl.java
@@ -0,0 +1,20 @@
+package com.company.books.backend.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.company.books.backend.response.CategoriaResponseRest;
+
+@Service
+public class CategoriaServicelmpl implements ICategoriaService{
+	
+	private static final Logger log = LoggerFactory.getLogger(CategoriaServicelmpl.class);
+	
+	@Override
+	public CategoriaResponseRest buscarCategorias() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/company/books/backend/services/ICategoriaService.java
@@ -0,0 +1,7 @@
+package com.company.books.backend.services;
+
+import com.company.books.backend.response.CategoriaResponseRest;
+
+public interface ICategoriaService {
+	public CategoriaResponseRest buscarCategorias();
+}
